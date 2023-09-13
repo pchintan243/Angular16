96611a84b4c0cd9d938329195b5c76fbf6dc329b
chintan <patelchintan843@gmail.com>
2023-09-13 19:30:42 +0530

    mutate method in signal

--- src/app/signal/signal.component.ts
+++ src/app/signal/signal.component.ts
@@ -8,15 +8,27 @@ import { Component, signal } from '@angular/core';
 export class SignalComponent {
   count = signal(0);
 
+  // We can give the type of data explicitly using diamond bracket.
+  message = signal<string[]>([]);
+
   inc() {
     // Set method is use when value is not depend on previous value.
     // this.count.set(this.count() + 1);
 
     // In such scenario if application depend on previous value at that time we will use update method.
     this.count.update((prev) => prev + 1);
+
+    // this.message.update((prevMsg) => [...prevMsg, "Message" + this.count()])
+
+    // When we dealing with an array use mutate method instead of update method.
+    // Update method create a new array every time and get the previous data first and after that push it.
+    // When we are dealing with immutable types like boolean, number, string at that time we use set or update method.
+    // When we dealing with objects and array at that time we use mutate method.
+    this.message.mutate((prevMsg) => prevMsg.push('' + this.count()))
   }
-  
+
   dec() {
-    this.count.update((prev) => prev - 1);
+    this.message.mutate((prevMsg) => prevMsg.pop())
+    // this.count.update((prev) => prev - 1);
   }
 }
